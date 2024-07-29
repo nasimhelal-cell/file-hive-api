4fde8a2c6b2c7a9b1ba6bfadc411af3f8974dadb
Nasim Helal <nasimhelal123@gmail.com>
2024-07-30 05:23:05 +0600

    Basic app setup with database connect and also add a /api-docs endpoint to visit swagger documentation

---
+++ db/connectDB.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+async function connectDB(server) {
+  const port = process.env.PORT || 4000;
+  const dbUrl = process.env.DB_URL;
+
+  try {
+    await mongoose.connect(dbUrl);
+    console.log("Database is connected !!! ✅");    
+    
+    server.listen(port, () => {
+      console.log("Server is listening at port: " + port);
+    });
+  } catch (error) {
+    console.log("Failed to connect with database! ❌");
+  }
+}
+
+module.exports = connectDB;
