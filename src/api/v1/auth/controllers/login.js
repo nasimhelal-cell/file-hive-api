62fff69bc287656d85fa40d40e9d6aed8f821419
nasim helal <nasimhelal123@gmail.com>
2024-08-09 23:25:21 +0600

    Login  logic and implementation with necessary services and also write  test case for each services and functions

--- src/api/v1/auth/controllers/login.js
+++ src/api/v1/auth/controllers/login.js
@@ -1,3 +1,21 @@
-const login = (req, res) => {};
+const { StatusCodes } = require("http-status-codes");
+const { catchAsync } = require("../../../../utils");
+const authServices = require("../../../../lib/auth");
+
+const login = catchAsync(async (req, res) => {
+  const { email, password } = req.body;
+
+  const token = await authServices.login({ email, password });
+
+  const response = {
+    code: StatusCodes.OK,
+    message: "Login successful",
+    data: {
+      token,
+    },
+  };
+
+  res.status(StatusCodes.OK).json(response);
+});
 
 module.exports = login;
